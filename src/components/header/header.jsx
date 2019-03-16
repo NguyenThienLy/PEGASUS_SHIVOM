c5a01304531ec98c5e3307b13e269eb46b587c72
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-03-16 07:22:28 +0700

    change header and profile ui

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,77 +3,84 @@ import './../../assets/bootstrap4/bootstrap.min.scss'
 
 import './header.scss'
 
+import Link from 'next/link'
+
 export class Header extends React.Component {
     constructor(props) {
         super(props)
     }
 
     render() {
-        console.log("props của header: ", this.props)
         return (
             <div className="headerWrap">
 
-            <nav className="navbar navbar-expand-lg navbar-light navbar-default navbar-fixed-top">
-                <a className="name navbar-brand">Book Review</a>
-                <button className="navbar-toggler fix-top" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="navWrap collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto mainMenu">
-                        <li className="nav-item current">
-                            <a href="#" className="nav-link active">Trang chủ</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Bài viết</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Thể loại</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Tác giả</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Reviewer</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Liên hệ</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
-                                ria-haspopup="true" aria-expanded="false">Xem thêm</a>
-                            <div className="dropdown-menu">
-                                <a href="#" className="dropdown-item">Nổi bật</a>
-                                <a href="#" className="dropdown-item">Bảng xếp hạng</a>
-                            </div>
-                        </li>
-                    </ul>
-
-                    <div className="accountWrap justify-content-end">
-
-                        <ul className="navbar-nav">
+                <nav className="navbar navbar-expand-lg navbar-light navbar-default navbar-fixed-top">
+                    <a className="name navbar-brand">Book Review</a>
+                    <button className="navbar-toggler fix-top" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="navWrap collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto mainMenu">
+                            <li className="nav-item current">
+                                <Link href="/">
+                                    <a className="nav-link active">Trang chủ</a>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href="/bai-viet">
+                                    <a className="nav-link">Bài viết</a>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href="/profile">
+                                    <a href="#" className="nav-link">Thể loại</a>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#" className="nav-link">Tác giả</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#" className="nav-link">Reviewer</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#" className="nav-link">Liên hệ</a>
+                            </li>
                             <li className="nav-item dropdown">
-                                <a href="#" className="nav-link dropdown-toggle" role="button" data-toggle="dropdown"
-                                    aria-expanded="false" aria-haspopup="true">Vi</a>
+                                <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                                    ria-haspopup="true" aria-expanded="false">Xem thêm</a>
                                 <div className="dropdown-menu">
-                                    <a href="#" className="dropdown-item">Tiếng Anh</a>
+                                    <a href="#" className="dropdown-item">Nổi bật</a>
+                                    <a href="#" className="dropdown-item">Bảng xếp hạng</a>
                                 </div>
                             </li>
-                            <li className="nav-item">
-                                <a href="#" className="nav-link">Đăng nhập</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="#" className="nav-link">Đăng ký</a>
-                            </li>
-
-
                         </ul>
+
+                        <div className="accountWrap justify-content-end">
+
+                            <ul className="navbar-nav">
+                                <li className="nav-item dropdown">
+                                    <a href="#" className="nav-link dropdown-toggle" role="button" data-toggle="dropdown"
+                                        aria-expanded="false" aria-haspopup="true">Vi</a>
+                                    <div className="dropdown-menu">
+                                        <a href="#" className="dropdown-item">Tiếng Anh</a>
+                                    </div>
+                                </li>
+                                <li className="nav-item">
+                                    <a href="#" className="nav-link">Đăng nhập</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a href="#" className="nav-link">Đăng ký</a>
+                                </li>
+
+
+                            </ul>
+                        </div>
                     </div>
-                    </div>
-            </nav>
-           
-        </div>
+                </nav>
+
+            </div>
         )
     }
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -15,10 +15,8 @@ class Home extends React.Component {
         super(props)
     }
     static async  getInitialProps({ req, query }) {
-        const res = await crudApi.getList()
-        console.log("posts : ", res.results.objects.rows)
         return {
-            
+
         }
     }
     render() {
@@ -27,13 +25,14 @@ class Home extends React.Component {
             <div>
                 <Head>
                     <title>Trang chủ</title>
+                    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/>
                     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
                         crossorigin="anonymous"></script>
-                        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
                         crossorigin="anonymous"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
                         crossorigin="anonymous"></script>
-                   
+                  
                 </Head>
                 <Header />
             </div>
