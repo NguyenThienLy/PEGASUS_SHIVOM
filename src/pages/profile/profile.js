4387388ac3a03ea3d02cd06edac5efef8865eeb3
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-03-16 08:35:59 +0700

    responsive for profile page

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -8,30 +8,28 @@ import './profile.scss'
 
 import { Info } from './components/info/info'
 
- class Profile extends React.Component {
+class Profile extends React.Component {
     constructor(props) {
         super(props)
     }
     static async  getInitialProps({ req, query }) {
         return {
-            
+
         }
     }
     render() {
-  
+
         return (
             <div>
                 <Head>
                     <title>Trang cá nhân</title>
-                    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/>
-                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-                        crossorigin="anonymous"></script>
-                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-                        crossorigin="anonymous"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-                        crossorigin="anonymous"></script>
+                    <meta content="width=device-width, initial-scale=1" name="viewport" />
+                    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
                 </Head>
-                <Header/>
+                <Header />
                 <div style={
                     {
                         height: "30px"
@@ -43,7 +41,7 @@ import { Info } from './components/info/info'
                         height: "20px"
                     }
                 }></div>
-                <Headline title="BÀI VIẾT REVIEW"/>
+                <Headline title="BÀI VIẾT REVIEW" />
             </div>
         )
     }
@@ -51,7 +49,7 @@ import { Info } from './components/info/info'
 
 const mapStateToProps = (state) => {
     return state;
-  };
-  
-  export default connect(mapStateToProps)(Profile);
+};
+
+export default connect(mapStateToProps)(Profile);
 
