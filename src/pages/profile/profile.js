99b0907ae465c772d9c8cbe2327882ad7936d396
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-03-22 23:08:34 +0700

    editor modal

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -7,15 +7,24 @@ import Link from 'next/link'
 import './profile.scss'
 
 import { Info } from './components/info/info'
+import { Editor } from './components/editor/editor'
 
 class Profile extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            showEditor: false
+        }
+        this.handleClose = this.handleClose.bind(this)
     }
     static async  getInitialProps({ req, query }) {
         return {
 
         }
+    }
+    async handleClose() {
+        console.log("close")
+        this.setState({ showEditor: false })
     }
     render() {
 
@@ -25,9 +34,9 @@ class Profile extends React.Component {
                     <title>Trang cá nhân</title>
                     <meta content="width=device-width, initial-scale=1" name="viewport" />
                     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
-                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+                        crossorigin="anonymous"></link>
+                  
                 </Head>
                 <Header />
                 <div style={
@@ -42,6 +51,7 @@ class Profile extends React.Component {
                     }
                 }></div>
                 <Headline title="BÀI VIẾT REVIEW" />
+                {this.state.showEditor?<Editor handleClose={this.handleClose} />:null}
             </div>
         )
     }
