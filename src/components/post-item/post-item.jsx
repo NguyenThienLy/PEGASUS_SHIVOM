423245675b69dd34c024c0802c6ace0771258a04
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-05-20 18:29:12 +0700

    intergate api for home

--- src/components/post-item/post-item.jsx
+++ src/components/post-item/post-item.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './post-item.scss'
+import * as moment from 'moment'
 export class PostItem extends Component {
     state = {}
     render() {
         const { post } = this.props
         return (
             <div id="post-item-wrap">
-                <div className="img"><a href="#"><img src={post.img} alt="" /></a></div>
+                <div className="img"><a href="#"><img src={post.thumb} alt="" /></a></div>
                 <div className="content">
                     <div className="title"><a href="#">{post.title}</a></div>
-                    <div className="time">{post.time}</div>
+                    <div className="time">{moment(post.createdAt).format("DD/MM/YYYY HH:mm")}</div>
                 </div>
             </div>
         );
