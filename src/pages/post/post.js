722cd511bb9bf3d1824e237396ee85139ea3d98d
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-06-18 10:25:54 +0700

    improve performance by remunnee uunnessecery fort import

--- src/pages/post/post.js
+++ src/pages/post/post.js
@@ -352,7 +352,6 @@ class Post extends React.Component {
             <div>
                 <Head>
                     <title>{this.props.post.title}</title>
-                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />
                 </Head>
                 <Header {...this.props} />
                 {this.state.loading ? <Loading /> :
