f12d25276195a3fd18384c6724c9e4be8eca10f5
Minh Le <minhle140198@gmail.com>
2019-08-20 19:01:22 +0700

    update ringingphone component and related post component

--- client/components/slider/slider.jsx
+++ client/components/slider/slider.jsx
@@ -1,47 +1,74 @@
-import * as React from 'react'
-import './slider.scss'
+import * as React from "react";
+import "./slider.scss";
 
-export class Slider extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render() {
-        return(
-            <div className="slider">
-            <div className="slider__slide slider__slide--active" data-slide="1">
-               <div className="slider__wrap">
-                 <div className="slider__back"></div>
-               </div>
-               <div className="slider__inner">
-                 <div className="slider__content">
-                   <h1>Slide <br/> One</h1><a className="go-to-next">next</a>
-                 </div>
-               </div>
-             </div>
-             <div className="slider__slide" data-slide="2">
-               <div className="slider__wrap">
-                 <div className="slider__back"></div>
-               </div>
-               <div className="slider__inner">
-                 <div className="slider__content">
-                   <h1>Slide <br/> Two</h1><a className="go-to-next">next</a>
-                 </div>
-               </div>
-             </div>
-             <div className="slider__slide" data-slide="3">
-               <div className="slider__wrap">
-                 <div className="slider__back"></div>
-               </div>
-               <div className="slider__inner">
-                 <div className="slider__content">
-                   <h1>Slide <br/> Three</h1><a className="go-to-next">next</a>
-                 </div>
-               </div>
-             </div>
-             <div className="slider__indicators"></div>
+export class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slideIndex: 0,
+      image: [
+        {
+          src:
+            "https://dalia.elated-themes.com/wp-content/uploads/2018/07/fitness-slider-1.jpg",
+          caption: "Caption one"
+        },
+        {
+          src:
+            "https://dalia.elated-themes.com/wp-content/uploads/2018/07/fitness-slider-2.jpg",
+          caption: "Caption two"
+        },
+        {
+          src:
+            "https://dalia.elated-themes.com/wp-content/uploads/2018/07/fitness-slider-3.jpg",
+          caption: "Caption three"
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="w3-content w3-display-container">
+        <img
+          className="mySlides"
+          src={this.state.image.src}
+          // style="width:100%"
+        />
+        <img
+          className="mySlides"
+          src={this.state.image.src}
+          // style="width:100%"
+        />
+        <img
+          className="mySlides"
+          src={this.state.image.src}
+          // style="width:100%"
+        />
+        <div
+          className="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle"
+          // style="width:100%"
+        >
+          <div className="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">
+            &#10094;
+          </div>
+          <div className="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">
+            &#10095;
+          </div>
+          <span
+            className="w3-badge demo w3-border w3-transparent w3-hover-white"
+            onclick="currentDiv(1)"
+          />
+          <span
+            className="w3-badge demo w3-border w3-transparent w3-hover-white"
+            onclick="currentDiv(2)"
+          />
+          <span
+            className="w3-badge demo w3-border w3-transparent w3-hover-white"
+            onclick="currentDiv(3)"
+          />
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-export default Slider
+export default Slider;
