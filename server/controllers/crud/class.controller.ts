6bc434cd7a4a5eb2e30b0e0ad7ee604dafba1da1
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-08-21 12:01:31 +0700

    api get time table of class, add time table item, delete timetable item

--- server/controllers/crud/class.controller.ts
+++ server/controllers/crud/class.controller.ts
@@ -1,10 +1,82 @@
+import * as _ from 'lodash'
+
 import { CrudController } from '../crud.controller'
-import { classService } from '../../services/index'
+import { classService, ICrudOption, classTimeTableService, timeTableItemService, studentTimeTableService, teacherService } from '../../services/index'
+import { ClassTimeTable, ClassModel, ClassTimeTableModel, TimeTableItemModel } from '../../models';
 
 
 export class ClassController extends CrudController<typeof classService>{
-    constructor(){
+    constructor() {
         super(classService);
     }
-    
+    async create(params: any, option?: ICrudOption) {
+        const classData: ClassModel = await this.service.create(params, option)
+        const classTimeTable: ClassTimeTableModel = await classTimeTableService.create({
+            class: classData._id,
+            course: classData.course,
+            name: classData.name
+        })
+        const json = classData.toJSON()
+        return _.merge({
+            timeTable: classTimeTable
+        }, json)
+    }
+    async getTimeTable(params: {
+        classId: string
+    }, option: ICrudOption) {
+        const { classId } = params
+        return await classTimeTableService.getItem(_.merge({
+            filter: {
+                class: classId
+            },
+            populates: ["items"]
+        }, option))
+    }
+    async addTimeTableItem(params: {
+        classId: string
+        timeTableItem: {
+            dayOfWeek: "monday" | "tuesday" | "webnesday" | "thursday" | "friday" | "saturday" | "sunday",
+            startTime: {
+                hour: number,
+                minute: number
+            },
+            endTime: {
+                hour: number,
+                minute: number
+            },
+            startAvailableCheckinTime: {
+                hour: number,
+                minute: number
+            },
+            endAvailabelCheckinTime?: {
+                hour: number,
+                minute: number
+            },
+            room: string
+        }
+    }) {
+        const { classId, timeTableItem } = params
+        const timeTableItemData: TimeTableItemModel = await timeTableItemService.create(timeTableItem)
+        return await classTimeTableService.update({
+            $push: { items: timeTableItemData._id }
+        }, {
+                filter: {
+                    class: classId
+                }
+            })
+    }
+    async deleteTimeTableItem(params: {
+        classId: string
+        timeTableItemId: string
+    }) {
+        const { classId, timeTableItemId } = params
+        return await timeTableItemService.update({
+            status: "deactive"
+        }, {
+                filter: {
+                    _id: timeTableItemId
+                }
+            })
+    }
+
 }
