25173dbd533e051711f0269916d7e87f678b831a
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-08-22 16:56:00 +0700

    fix code generate for new code structure

---
+++ devHelper/templates/model/services/crud/{component}.service.ts
@@ -0,0 +1,8 @@
+import { CrudService } from '../crud.service'
+import { {Name}, {Name}Model } from '../../models'
+
+export class {Name} Service extends CrudService <typeof {Name} > {
+    constructor() {
+        super({Name});
+    }
+}
--- devHelper/templates/model/services/crud/{component}Service.ts
+++
@@ -1,8 +0,0 @@
-import { CrudService } from '../crudService'
-import { {Name}, {Name}Model } from '../../models'
-
-export class {Name}Service extends CrudService<typeof {Name}> {
-    constructor(){
-        super({Name});
-    }
-}
