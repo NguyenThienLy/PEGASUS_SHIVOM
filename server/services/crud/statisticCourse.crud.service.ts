3f2eb05c0dfd8e3452fb09c323fc83c333ebf9e8
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-08-27 18:37:25 +0700

    get data for statisticpie of course

--- server/services/crud/statisticCourse.crud.service.ts
+++ server/services/crud/statisticCourse.crud.service.ts
@@ -26,8 +26,7 @@ export class StatisticCourseService extends CrudService<typeof StatisticCourse> 
             {
                 $project: {
                     _id: 0,
-                    type: 1,
-                    colStatictis: {
+                    labels: {
                         $switch: {
                             branches: [
                                 { case: { $and: [{ $eq: ["$type", "week"] }, { $eq: [params.type, "week"] }] }, then: "$time.week" },
@@ -62,12 +61,74 @@ export class StatisticCourseService extends CrudService<typeof StatisticCourse> 
             },
             // Sắp xếp theo thời gian
             {
-                $sort: { time: 1 }
+                $sort: { labels: 1 }
             },
             // Loại bỏ field khi dùng xong
             {
                 $project: {
                     time: 0
+                }
+            }
+        ])
+    }
+
+    async getDateInStatisticPie(params: {
+        course: string,
+        type: "week" | "month" | "year",
+        totalWeekStartTime: number,
+        totalWeekEndTime: number
+    }) {
+
+        return await this.model.aggregate([
+            // Lọc ra theo khóa học và type phù hợp
+            {
+                $match: {
+                    course: new ObjectId(params.course),
+                    type: params.type
+                }
+            },
+            // Lấy ra các thuộc tính cần thiết
+            {
+                $project: {
+                    _id: 0,
+                    time: {
+                        $switch: {
+                            branches: [
+                                { case: { $and: [{ $eq: ["$type", "week"] }, { $eq: [params.type, "week"] }] }, then: { $add: ["$time.week", { $multiply: ["$time.year", 52] }] } },
+                                { case: { $and: [{ $eq: ["$type", "month"] }, { $eq: [params.type, "month"] }] }, then: { $add: [{ $multiply: [{ $subtract: ["$time.month", 1] }, 4] }, { $multiply: ["$time.year", 52] }] } },
+                                { case: { $and: [{ $eq: ["$type", "year"] }, { $eq: [params.type, "year"] }] }, then: { $multiply: ["$time.year", 52] } }
+                            ]
+                        }
+                    },
+                    totalAbsent: 1,
+                    totalLate: 1,
+                    totalOnTime: 1,
+                    totalRedundant: 1
+                }
+            },
+            // Lọc ra các giá trị nằm trong khoảng thời gian
+            {
+                $match: {
+                    time: {
+                        $gte: params.totalWeekStartTime,
+                        $lte: params.totalWeekEndTime
+                    }
+                }
+            },
+            //Tính theo tổng hết tất cả field
+            {
+                $group: {
+                    _id: null,
+                    totalAbsent: { $sum: "$totalAbsent" },
+                    totalLate: { $sum: "$totalLate" },
+                    totalOnTime: { $sum: "$totalOnTime" },
+                    totalRedundant: { $sum: "totalRedundant" }
+                }
+            },
+            // Loại bỏ field khi dùng xong
+            {
+                $project: {
+                    _id: 0
                 }
             }
         ])
