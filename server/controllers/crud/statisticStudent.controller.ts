99bfb04316ce48e7ba376caaa95470e0e02d581a
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-09-01 15:31:45 +0700

    get data for calendar chart of student

--- server/controllers/crud/statisticStudent.controller.ts
+++ server/controllers/crud/statisticStudent.controller.ts
@@ -62,4 +62,98 @@ export class StatisticStudentController extends CrudController<typeof statisticS
             })
         }
     }
+
+    async statisticForCalendarChart(params: {
+        student: string,
+        course: string,
+        type: "week" | "month" | "year",
+        startTime: Date,
+        endTime: Date
+    }) {
+        // Lay ma hash duy nhat de query cache
+        const hashCode = hash(JSON.stringify(params))
+        // Lay du lieu trong cache
+        const cacheData = await cacheService.get(hashCode)
+
+        if (cacheData) {
+            return cacheData
+        } else {
+            const { student, course, type, startTime, endTime } = params
+            let totalWeekStartTime, totalWeekEndTime
+
+            // Đổi sang tuần theo type
+            switch (type) {
+                case "week":
+                    totalWeekStartTime = moment(startTime).week() + moment(startTime).year() * 52
+                    totalWeekEndTime = moment(endTime).week() + moment(endTime).year() * 52
+                    break
+
+                case "month":
+                    totalWeekStartTime = (moment(startTime).month() - 1) * 4 + moment(startTime).year() * 52
+                    totalWeekEndTime = (moment(endTime).month() - 1) * 4 + moment(endTime).year() * 52
+                    break
+
+                case "year":
+                    totalWeekStartTime = moment(startTime).year() * 52
+                    totalWeekEndTime = moment(endTime).year() * 52
+                    break
+            }
+
+            // Thống kê theo kiểu week, month, year
+            return this.service.getDateInStatisticForCalendarChart({
+                student,
+                course,
+                type,
+                totalWeekStartTime,
+                totalWeekEndTime
+            })
+        }
+    }
+
+    async statisticForPieChart(params: {
+        student: string,
+        course: string,
+        type: "week" | "month" | "year",
+        startTime: Date,
+        endTime: Date
+    }) {
+        // Lay ma hash duy nhat de query cache
+        const hashCode = hash(JSON.stringify(params))
+        // Lay du lieu trong cache
+        const cacheData = await cacheService.get(hashCode)
+
+        if (cacheData) {
+            return cacheData
+        } else {
+            const { student, course, type, startTime, endTime } = params
+            let totalWeekStartTime, totalWeekEndTime
+
+            // Đổi sang tuần theo type
+            switch (type) {
+                case "week":
+                    totalWeekStartTime = moment(startTime).week() + moment(startTime).year() * 52
+                    totalWeekEndTime = moment(endTime).week() + moment(endTime).year() * 52
+                    break
+
+                case "month":
+                    totalWeekStartTime = (moment(startTime).month() - 1) * 4 + moment(startTime).year() * 52
+                    totalWeekEndTime = (moment(endTime).month() - 1) * 4 + moment(endTime).year() * 52
+                    break
+
+                case "year":
+                    totalWeekStartTime = moment(startTime).year() * 52
+                    totalWeekEndTime = moment(endTime).year() * 52
+                    break
+            }
+
+            // Thống kê theo kiểu week, month, year
+            return this.service.getDateInStatisticForPieChart({
+                student,
+                course,
+                type,
+                totalWeekStartTime,
+                totalWeekEndTime
+            })
+        }
+    }
 }
--- server/services/crud/statisticStudent.crud.service.ts
+++ server/services/crud/statisticStudent.crud.service.ts
@@ -2,6 +2,7 @@ import { CrudService } from '../crud.service'
 import { StatisticStudent } from '../../models'
 import { ObjectId } from 'bson'
 import { format } from 'path';
+import * as _ from 'lodash'
 
 export class StatisticStudentService extends CrudService<typeof StatisticStudent> {
     constructor() {
@@ -69,8 +70,8 @@ export class StatisticStudentService extends CrudService<typeof StatisticStudent
                                     case: { $eq: [params.format, "absent"] }, then: {
                                         $filter: {
                                             input: "$absent",
-                                            as: "absent",
-                                            cond: { $eq: ["$$absent.course", new ObjectId(params.course)] }
+                                            as: "element",
+                                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
                                         }
                                     }
                                 },
@@ -78,17 +79,17 @@ export class StatisticStudentService extends CrudService<typeof StatisticStudent
                                     case: { $eq: [params.format, "late"] }, then: {
                                         $filter: {
                                             input: "$late",
-                                            as: "late",
-                                            cond: { $eq: ["$$late.course", new ObjectId(params.course)] }
+                                            as: "element",
+                                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
                                         }
                                     }
                                 },
                                 {
                                     case: { $eq: [params.format, "on_time"] }, then: {
                                         $filter: {
-                                            input: "$on_time",
-                                            as: "on_time",
-                                            cond: { $eq: ["$$on_time.course", new ObjectId(params.course)] }
+                                            input: "$onTime",
+                                            as: "element",
+                                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
                                         }
                                     }
                                 },
@@ -96,8 +97,8 @@ export class StatisticStudentService extends CrudService<typeof StatisticStudent
                                     case: { $eq: [params.format, "redundant"] }, then: {
                                         $filter: {
                                             input: "$redundant",
-                                            as: "redundant",
-                                            cond: { $eq: ["$$redundant.course", new ObjectId(params.course)] }
+                                            as: "element",
+                                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
                                         }
                                     }
                                 }
@@ -133,6 +134,219 @@ export class StatisticStudentService extends CrudService<typeof StatisticStudent
                     _id: 0,
                     time: 0,
                     "formats.course": 0
+                }
+            }
+        ])
+    }
+
+    async getDateInStatisticForCalendarChart(params: {
+        student: string,
+        course: string,
+        type: "week" | "month" | "year",
+        totalWeekStartTime: number,
+        totalWeekEndTime: number
+    }) {
+        let timeLine = []
+
+        const tempTimeLine = await this.model.aggregate([
+            // Lọc ra theo khóa học và type phù hợp
+            {
+                $match: {
+                    type: params.type,
+                    student: new ObjectId(params.student)
+                }
+            },
+            // Lấy ra các thuộc tính cần thiết
+            {
+                $project: {
+                    student: 1,
+                    absent: {
+                        // Lọc theo lớp học
+                        $filter: {
+                            input: "$absent",
+                            as: "element",
+                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
+                        }
+                    },
+                    late: {
+                        $filter: {
+                            input: "$late",
+                            as: "element",
+                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
+                        }
+                    },
+                    onTime: {
+                        $filter: {
+                            input: "$onTime",
+                            as: "element",
+                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
+                        }
+                    },
+                    redundant: {
+                        $filter: {
+                            input: "$redundant",
+                            as: "element",
+                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
+                        }
+                    },
+                    time: {
+                        $switch: {
+                            branches: [
+                                { case: { $and: [{ $eq: ["$type", "week"] }, { $eq: [params.type, "week"] }] }, then: { $add: ["$time.week", { $multiply: ["$time.year", 52] }] } },
+                                { case: { $and: [{ $eq: ["$type", "month"] }, { $eq: [params.type, "month"] }] }, then: { $add: [{ $multiply: [{ $subtract: ["$time.month", 1] }, 4] }, { $multiply: ["$time.year", 52] }] } },
+                                { case: { $and: [{ $eq: ["$type", "year"] }, { $eq: [params.type, "year"] }] }, then: { $multiply: ["$time.year", 52] } }
+                            ]
+                        }
+                    }
+                }
+            },
+            // Lọc ra các giá trị nằm trong khoảng thời gian và loại bỏ các phần tử
+            {
+                $match: {
+                    time: {
+                        $gte: params.totalWeekStartTime,
+                        $lte: params.totalWeekEndTime
+                    }
+                }
+            },
+            // Group theo nhóm
+            {
+                $group: {
+                    _id: "$student",
+                    absent: { $push: "$absent.time" },
+                    late: { $push: "$late.time" },
+                    onTime: { $push: "$onTime.time" },
+                    redundant: { $push: "$redundant.time" },
+                }
+            },
+            // Bỏ id
+            {
+                $project: {
+                    _id: 0
+                }
+            }
+        ])
+
+        // Đẩy element type absent vào arr timeLine
+        _.map(_.flatten(tempTimeLine[0].absent), function (item) {
+            timeLine.push({ time: item, type: "absent" })
+        })
+
+        // Đẩy element type late vào arr timeLine
+        _.map(_.flatten(tempTimeLine[0].late), function (item) {
+            timeLine.push({ time: item, type: "late" })
+        })
+
+        // Đẩy element type on_time vào arr timeLine
+        _.map(_.flatten(tempTimeLine[0].onTime), function (item) {
+            timeLine.push({ time: item, type: "on_time" })
+        })
+
+        // Đẩy element type redundant vào arr timeLine
+        _.map(_.flatten(tempTimeLine[0].redundant), function (item) {
+            timeLine.push({ time: item, type: "redundant" })
+        })
+
+        return {
+            absents: _.flatten(tempTimeLine[0].absent),
+            lates: _.flatten(tempTimeLine[0].late),
+            onTimes: _.flatten(tempTimeLine[0].onTime),
+            redundants: _.flatten(tempTimeLine[0].redundant),
+            timeLines: timeLine
+        }
+    }
+
+    async getDateInStatisticForPieChart(params: {
+        student: string,
+        course: string,
+        type: "week" | "month" | "year",
+        totalWeekStartTime: number,
+        totalWeekEndTime: number
+    }) {
+
+        console.log(params)
+
+        return await this.model.aggregate([
+            // Lọc ra theo khóa học và type phù hợp
+            {
+                $match: {
+                    type: params.type,
+                    student: new ObjectId(params.student)
+                }
+            },
+            // Lấy ra các thuộc tính cần thiết
+            {
+                $project: {
+                    student: 1,
+                    absent: {
+                        // Lọc theo lớp học
+                        $filter: {
+                            input: "$absent",
+                            as: "element",
+                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
+                        }
+                    },
+                    late: {
+                        $filter: {
+                            input: "$late",
+                            as: "element",
+                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
+                        }
+                    },
+                    onTime: {
+                        $filter: {
+                            input: "$onTime",
+                            as: "element",
+                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
+                        }
+                    },
+                    redundant: {
+                        $filter: {
+                            input: "$redundant",
+                            as: "element",
+                            cond: { $eq: ["$$element.course", new ObjectId(params.course)] }
+                        }
+                    },
+                    time: {
+                        $switch: {
+                            branches: [
+                                { case: { $and: [{ $eq: ["$type", "week"] }, { $eq: [params.type, "week"] }] }, then: { $add: ["$time.week", { $multiply: ["$time.year", 52] }] } },
+                                { case: { $and: [{ $eq: ["$type", "month"] }, { $eq: [params.type, "month"] }] }, then: { $add: [{ $multiply: [{ $subtract: ["$time.month", 1] }, 4] }, { $multiply: ["$time.year", 52] }] } },
+                                { case: { $and: [{ $eq: ["$type", "year"] }, { $eq: [params.type, "year"] }] }, then: { $multiply: ["$time.year", 52] } }
+                            ]
+                        }
+                    }
+                }
+            },
+            // Lọc ra các giá trị nằm trong khoảng thời gian và loại bỏ các phần tử
+            {
+                $match: {
+                    time: {
+                        $gte: params.totalWeekStartTime,
+                        $lte: params.totalWeekEndTime
+                    }
+                }
+            },
+            {
+                totalAbsent: { $sum: "$absent" },
+                totalLate: { $sum: "$late" },
+                totalOnTime: { $sum: "$onTime" },
+                totalRedundat: { $sum: "$Redundant" },
+            },
+            // Group theo nhóm
+            {
+                $group: {
+                    _id: "$student",
+                    absent: { $push: "$absent.time" },
+                    late: { $push: "$late.time" },
+                    onTime: { $push: "$onTime.time" },
+                    redundant: { $push: "$redundant.time" },
+                }
+            },
+            // Bỏ id
+            {
+                $project: {
+                    _id: 0
                 }
             }
         ])
