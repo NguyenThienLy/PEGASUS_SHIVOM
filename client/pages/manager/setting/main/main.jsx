6e384198e52d2e2df80156d4e03fa5ae85297df6
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-10-22 22:07:00 +0700

    setting manager loadding when fetching data

--- client/pages/manager/setting/main/main.jsx
+++ client/pages/manager/setting/main/main.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Form } from '../../../../components'
+import { Form, Loading } from '../../../../components'
 import { action } from '../../../../actions';
 import { api } from '../../../../services';
 import Swal from 'sweetalert2';
@@ -51,150 +51,152 @@ export class MainSetting extends React.Component {
             })
     }
     render() {
-        console.log("setting: ", this.props.setting)
         return (
-            <React.Fragment>
-                <Form
-                    title="Thiết lập logo website"
-                    form={{
-                        logo: {
-                            type: "image",
-                            label: "Logo",
-                            placeholder: "Tải lên logo",
-                            value: "",
-                            isValid: false,
-                            errorMessage: ""
-                        }
+            this.props.setting.fetching ? <Loading /> :
+                <React.Fragment>
 
-                    }}
-                    validate={{
-                        rules: {
+                    <Form
+                        title="Thiết lập logo website"
+                        form={{
                             logo: {
-                                required: true
+                                type: "image",
+                                label: "Logo",
+                                placeholder: "Tải lên logo",
+                                value: "",
+                                isValid: false,
+                                errorMessage: ""
                             }
-                        },
-                        messages: {
-                            logo: {
-                                required: "Bắt buộc phải có ảnh"
+
+                        }}
+                        validate={{
+                            rules: {
+                                logo: {
+                                    required: true
+                                }
+                            },
+                            messages: {
+                                logo: {
+                                    required: "Bắt buộc phải có ảnh"
+                                }
                             }
-                        }
-                    }}
-                    submit={this.submitLogo}
-                />
-                <hr />
-                <Form
-                    title="Thiết lập thông tin liên hệ"
-                    form={{
-                        brand: {
-                            type: "text",
-                            label: "Brand",
-                            defaultValue: this.props.setting.contact.brand,
-                            value: this.props.setting.contact.brand,
-                            isValid: true,
-                            errorMessage: ""
-                        },
-                        title: {
-                            type: "text",
-                            label: "Tiêu đề",
-                            defaultValue: this.props.setting.contact.title,
-                            value: this.props.setting.contact.title,
-                            isValid: true,
-                            errorMessage: ""
-                        },
-                        description: {
-                            type: "text",
-                            label: "Mô tả",
-                            defaultValue: this.props.setting.contact.description,
-                            value: this.props.setting.contact.description,
-                            isValid: true,
-                            errorMessage: ""
-                        },
-                        phone: {
-                            type: "text",
-                            label: "Số điện thoại",
-                            defaultValue: this.props.setting.contact.phone,
-                            value: this.props.setting.contact.phone,
-                            isValid: true,
-                            errorMessage: ""
-                        },
-                        email: {
-                            type: "text",
-                            label: "Email",
-                            defaultValue: this.props.setting.contact.email,
-                            value: this.props.setting.contact.email,
-                            isValid: true,
-                            errorMessage: ""
-                        },
-                        address: {
-                            type: "text",
-                            label: "Địa chỉ",
-                            defaultValue: this.props.setting.contact.address,
-                            value: this.props.setting.contact.address,
-                            isValid: true,
-                            errorMessage: ""
-                        },
+                        }}
+                        submit={this.submitLogo}
+                    />
+                    <hr />
+                    <Form
+                        title="Thiết lập thông tin liên hệ"
+                        form={{
+                            brand: {
+                                type: "text",
+                                label: "Brand",
+                                defaultValue: this.props.setting.contact.brand,
+                                value: this.props.setting.contact.brand,
+                                isValid: true,
+                                errorMessage: ""
+                            },
+                            title: {
+                                type: "text",
+                                label: "Tiêu đề",
+                                defaultValue: this.props.setting.contact.title,
+                                value: this.props.setting.contact.title,
+                                isValid: true,
+                                errorMessage: ""
+                            },
+                            description: {
+                                type: "text",
+                                label: "Mô tả",
+                                defaultValue: this.props.setting.contact.description,
+                                value: this.props.setting.contact.description,
+                                isValid: true,
+                                errorMessage: ""
+                            },
+                            phone: {
+                                type: "text",
+                                label: "Số điện thoại",
+                                defaultValue: this.props.setting.contact.phone,
+                                value: this.props.setting.contact.phone,
+                                isValid: true,
+                                errorMessage: ""
+                            },
+                            email: {
+                                type: "text",
+                                label: "Email",
+                                defaultValue: this.props.setting.contact.email,
+                                value: this.props.setting.contact.email,
+                                isValid: true,
+                                errorMessage: ""
+                            },
+                            address: {
+                                type: "text",
+                                label: "Địa chỉ",
+                                defaultValue: this.props.setting.contact.address,
+                                value: this.props.setting.contact.address,
+                                isValid: true,
+                                errorMessage: ""
+                            },
 
-                    }}
-                    validate={{
-                        rules: {
+                        }}
+                        validate={{
+                            rules: {
 
-                        },
-                        messages: {
+                            },
+                            messages: {
 
-                        }
-                    }}
-                    submit={this.submitContact}
-                />
-                <hr />
-                <Form
-                    title="Thiết lập liên kết mạng xã hội"
-                    form={{
-                        facebook: {
-                            type: "text",
-                            label: "Facebook",
-                            defaultValue: this.props.setting.social.facebook,
-                            value: this.props.setting.social.facebook,
-                            isValid: true,
-                            errorMessage: ""
-                        },
-                        google: {
-                            type: "text",
-                            label: "Google plus",
-                            defaultValue: this.props.setting.social.google,
-                            value: this.props.setting.social.google,
-                            isValid: true,
-                            errorMessage: ""
-                        },
-                        instagram: {
-                            type: "text",
-                            label: "Instagram",
-                            defaultValue: this.props.setting.social.instagram,
-                            value: this.props.setting.social.instagram,
-                            isValid: true,
-                            errorMessage: ""
-                        },
-                        youtube: {
-                            type: "text",
-                            label: "Youtube",
-                            defaultValue: this.props.setting.social.youtube,
-                            value: this.props.setting.social.youtube,
-                            isValid: true,
-                            errorMessage: ""
-                        }
+                            }
+                        }}
+                        submit={this.submitContact}
+                    />
+                    <hr />
+                    <Form
+                        title="Thiết lập liên kết mạng xã hội"
+                        form={{
+                            facebook: {
+                                type: "text",
+                                label: "Facebook",
+                                defaultValue: this.props.setting.social.facebook,
+                                value: this.props.setting.social.facebook,
+                                isValid: true,
+                                errorMessage: ""
+                            },
+                            google: {
+                                type: "text",
+                                label: "Google plus",
+                                defaultValue: this.props.setting.social.google,
+                                value: this.props.setting.social.google,
+                                isValid: true,
+                                errorMessage: ""
+                            },
+                            instagram: {
+                                type: "text",
+                                label: "Instagram",
+                                defaultValue: this.props.setting.social.instagram,
+                                value: this.props.setting.social.instagram,
+                                isValid: true,
+                                errorMessage: ""
+                            },
+                            youtube: {
+                                type: "text",
+                                label: "Youtube",
+                                defaultValue: this.props.setting.social.youtube,
+                                value: this.props.setting.social.youtube,
+                                isValid: true,
+                                errorMessage: ""
+                            }
 
-                    }}
-                    validate={{
-                        rules: {
+                        }}
+                        validate={{
+                            rules: {
 
-                        },
-                        messages: {
+                            },
+                            messages: {
 
-                        }
-                    }}
-                    submit={this.submitSocial}
-                />
+                            }
+                        }}
+                        submit={this.submitSocial}
+                    />
 
-            </React.Fragment>
+                </React.Fragment>
+
         );
     }
 }
